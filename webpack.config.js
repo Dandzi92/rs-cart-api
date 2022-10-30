2d5becd8537027bf62e83d567d50e5407b0e9584
dzianis danilkou <d.danilkov@dialmyapp.com>
2022-10-30 22:46:50 +0300

    added serverless config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+module.exports = (options, webpack) => {
+    const lazyImports = [
+        '@nestjs/microservices/microservices-module',
+        '@nestjs/websockets/socket-module',
+    ];
+
+    const TerserPlugin = require('terser-webpack-plugin');
+
+    console.log(webpack, 'Webpack')
+
+    return {
+        ...options,
+        externals: [],
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        keep_classnames: true,
+                    },
+                }),
+            ],
+        },
+        output: {
+            ...options.output,
+            libraryTarget: 'commonjs2',
+        },
+        plugins: [
+            ...options.plugins,
+            new webpack.IgnorePlugin({
+                resourceRegExp: /^pg-native$/,
+                checkResource(resource) {
+                    if (lazyImports.includes(resource)) {
+                        try {
+                            require.resolve(resource);
+                        } catch (err) {
+                            return true;
+                        }
+                    }
+                    return false;
+                },
+            }),
+        ],
+    };
+};
